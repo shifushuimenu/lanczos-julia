a1ab8a42c238be97df5ee486a162bd849e91b8fb
aloma <aloma@kabir>
2022-01-05 18:55:27 +0100

    two-pass Lanczos working correctly

--- Lanczos.jl
+++ Lanczos.jl
@@ -1,20 +1,52 @@
 # TODO: - extend to complex numbers 
 #       - struct for Krylov space 
+#       - two-pass Lanczos with only two and three Lanczos states (without re-orthogonalization)
+#       - Documentation
+#       - Measurements of diagonal and off-diagonal observables 
 # Later: 
-#       - entanglement entropy
+#       - time evolution 
+#       - calculate Floquet spectrum by time evolution 
+#       - finite temperature, response functions
+#       - arbitrary lattices 
+#       - Rydberg systems, i.e. Hilbert space sectors  
+#       - Kanamori Hamiltonian 
+#       - entanglement entropy for detecting phase boundaries 
 
 using LinearAlgebra
+
+"normalize a vector in place"
+function normalize!(v::Vector{T}) where T<:Float64
+    norm = 0.0
+    for i in 1:length(v)
+        norm += v[i]*v[i]
+    end 
+    norm = sqrt(norm)
+    v[:] = v[:] / norm 
+    return norm 
+end 
+
+"normalize column `col` of a 2-dim array in place"
+function normalize!(v::AbstractArray{T, 2}, col::Int) where T<:Float64
+    norm = 0.0
+    for i in 1:length(v[:, col])
+        norm += v[i, col]*v[i, col]
+    end 
+    norm = sqrt(norm)
+    v[:, col] = v[:, col] / norm 
+    return norm 
+end 
 
 abstract type SparseSymmetricMatrix end
 
 struct SparseHamiltonian{iT, fT} <: SparseSymmetricMatrix 
-    n::iT       # dim of Hilbert space 
+    n::iT          # dim of Hilbert space 
     B::Vector{iT}  # B stores the positions of non-zero matrix elements in the respective rows of H 
     E::Vector{iT}  # E stores for each state s the number of non-zero matrix elements <s'|H|s>
     H::Vector{fT}  # Matrix elements in the same succession as stored in B 
 end 
 
 SparseHamiltonian(N::Int) = SparseHamiltonian(Int64, Float64, N)
+
 function SparseHamiltonian(iT::Type, fT::Type, N::Int)
     n = 2^N
     num_matrix_el = n+Int(n//2)*N
@@ -29,6 +61,7 @@ abstract type AbstractKrylovSubspace end
 
 struct KrylovSubspace{rT, cT} <: AbstractKrylovSubspace 
     m::Int 
+    store_Krylov_states::Bool
     Q::Array{cT, 2}
     α::Vector{rT}
     β::Vector{rT}
@@ -36,13 +69,22 @@ struct KrylovSubspace{rT, cT} <: AbstractKrylovSubspace
     ψ0::Vector{cT}
 end 
 
-function KrylovSubspace(rT::Type, n::Int, m::Int, ψ0::AbstractVector) 
+"""
+   n (Int): dimension of the Hilbert space 
+   m (Int): dimension of the Krylov subspace 
+   num_Lanczos_vecs (Int): 
+"""
+function KrylovSubspace(rT::Type, n::Int, m::Int, ψ0::AbstractVector; store_Krylov_states::Bool=true)
     cT = rT  # IMPROVE: cT = complex(rT)
-    Q = Array{rT, 2}(undef, (n, m+1))
+    if store_Krylov_states
+       Q = Array{rT, 2}(undef, (n, m+1))
+    else 
+       Q = Array{rT, 2}(undef, (n, 3))
+    end
     α = Vector{rT}(undef, m)
     β = Vector{rT}(undef, m+1)
     L = Array{rT, 2}(undef, (m, m))
-    KrylovSubspace{rT, cT}(m, Q, α, β, L, ψ0)
+    KrylovSubspace{rT, cT}(m, store_Krylov_states, Q, α, β, L, ψ0)
 end 
 
 
@@ -59,7 +101,8 @@ function hinit(J::Array{T, 2}; hx=1.0, hz=1.0) where T<:Float64
         e = 0
 
         # 1. diagonal matrix elements 
-        spins = [(s & (1<<site_k) == 0) ? -1 : 1 for site_k in 0:N-1]
+        spins = [(s & (1<<site_k) == 0) ? -1 : 1
+         for site_k in 0:N-1]
         intsum = sum([(J[k,l]*spins[k]*spins[l]) for k in 1:N for l in 1:k-1])
         h.B[i] = s + 1  # one-indexing
         h.H[i] = (intsum - sum(spins) * hz) / 2.0  # later on diagonal matrix elements are double counted, therefore we divide by 2 here.
@@ -99,39 +142,111 @@ function hoperation!(h::SparseHamiltonian{Int64, T}, phi::Vector{T}, gamma::Vect
 end 
 
 
-"normalize a vector in place"
-function normalize!(v::Vector{T}) where T<:Float64
-    norm = 0.0
-    for i in 1:length(v)
-        norm += v[i]*v[i]
-    end 
-    norm = sqrt(norm)
-    v[:] = v[:] / norm 
-    return norm 
-end 
-
-"normalize column `col` of a 2-dim array in place"
-function normalize!(v::AbstractArray{T, 2}, col::Int) where T<:Float64
-    norm = 0.0
-    for i in 1:length(v[:, col])
-        norm += v[i, col]*v[i, col]
-    end 
-    norm = sqrt(norm)
-    v[:, col] = v[:, col] / norm 
-    return norm 
-end 
-
 mutable struct LanczosInfo 
-    qmin::Int  # minimum number of Lanczos vectors; on output: number of Lanczos states actually needed for convergence 
-    qmax::Int  # maximum number of Lanczos vectors 
+    qmin::Int  # minimum number of Lanczos vectors/iterations; on output: number of Lanczos states/iterations actually needed for convergence 
+    qmax::Int  # maximum number of Lanczos vectors/iterations
     converged::Bool # if error < eps
     error::Float64  # error, if error > eps 
 end 
 
+
+"""
+Lanczos algorithm using as little memory as possible.
+Two Lanczos vectors are needed to get ground state energy,
+three Lanczos vectors are needed for ground state vector.
+Reference: https://www.cond-mat.de/events/correl11/manuscript/Koch.pdf
+"""
+function lanczos_twopass(h::SparseHamiltonian{Int64, T}, v_init::Vector{T}, g_out::Vector{T}, 
+    info::LanczosInfo; eps=1e-13) where T<:Float64
+
+    length(g_out) == length(v_init) || error("size mismatch")
+    n = length(v_init)
+    if info.qmax > n
+        info.qmax = convert(Int, n)
+    end 
+
+    Ks = KrylovSubspace(Float64, n, info.qmax, v_init; store_Krylov_states=false)
+
+    energies = Vector{Float64}(undef, info.qmax)
+
+    # generate the first two Lanczos states
+    normalize!(v_init)
+    Ks.Q[:, 1] = v_init    
+    Ks.Q[:, 2] = hoperation!(h, Ks.Q[:,1], Ks.Q[:,2])
+    Ks.α[1] = dot(Ks.Q[:,1], Ks.Q[:,2])
+    Ks.Q[:, 2] -= Ks.α[1] * Ks.Q[:,1]
+    Ks.β[2] = normalize!(Ks.Q, 2)
+
+    # generate the rest of the Lanczos basis
+    e_prev = 0.0
+    for m = 2:info.qmax
+
+        Ks.Q[:,3] = hoperation!(h, Ks.Q[:,2], Ks.Q[:,3])
+        Ks.α[m] = dot(Ks.Q[:,2], Ks.Q[:,3])
+
+        tri = SymTridiagonal(Ks.α[1:m], Ks.β[2:m]) 
+        F = eigen(tri)
+        Ks.L[1:m, 1:m] = F.vectors
+        energies[1:m] = F.values
+        perm = sortperm(energies[1:m])
+        energies[1:m] = energies[perm[1:m]]
+        Ks.L[1:m, 1:m] = Ks.L[1:m, perm[1:m]]
+
+        e = energies[1]
+        println("ground state energy=", energies[1])
+        error = abs((e_prev-e)/e)
+        info.error = error
+        if error < eps
+            info.converged = true 
+            break 
+        else 
+            e_prev = e 
+        end 
+
+        info.qmin = m # Number of actually generated Lanczos vectors 
+
+        Ks.Q[:, 3] -= Ks.α[m] .* Ks.Q[:, 2] .+ Ks.β[m]*Ks.Q[:, 1]
+        Ks.β[m+1] = normalize!(Ks.Q, 3)            
+
+        Ks.Q[:, 1] = Ks.Q[:, 2] 
+        Ks.Q[:, 2] = Ks.Q[:, 3]
+
+    end 
+
+    # Build the ground state. Re-generate the Krylov states (because they were not stored 
+    # due to memory constraints).
+    fill!(g_out, 0.0)
+    g_out[:] = g_out[:] +  Ks.L[1, 1] * v_init[:]
+
+    Ks.Q[:,1] = v_init    
+    Ks.Q[:,2] = hoperation!(h, Ks.Q[:,1], Ks.Q[:,2])    
+    Ks.Q[:,2] -= Ks.α[1] * Ks.Q[:,1]
+    normalize!(Ks.Q, 2)
+
+    for m = 2:info.qmin 
+        vv = Ks.Q[:, 2]
+        g_out[:] = g_out[:] + Ks.L[m, 1] * vv[:]
+
+        if (m < info.qmin)
+           Ks.Q[:,3] = hoperation!(h, Ks.Q[:,2], Ks.Q[:,3])
+           Ks.Q[:, 3] -= Ks.α[m] .* Ks.Q[:, 2] .+ Ks.β[m]*Ks.Q[:, 1]
+           normalize!(Ks.Q, 3)
+
+           Ks.Q[:, 1] = Ks.Q[:, 2] 
+           Ks.Q[:, 2] = Ks.Q[:, 3]
+        end 
+    end 
+    
+    normalize!(g_out) # should be normalized already 
+    return g_out 
+
+end 
+
+
 """"return ground state vector and Krylov subspace object 
  - v: random input vector on which the Krylov space is built
  - g_out: allocated vector into which the normalized ground state is written"""
-function lanczos_sparse(h::SparseHamiltonian{Int64, T}, v::Vector{T}, g_out::Vector{T}, 
+function lanczos_reortho(h::SparseHamiltonian{Int64, T}, v::Vector{T}, g_out::Vector{T}, 
     info::LanczosInfo; eps=1e-13) where T<:Float64
 
     length(g_out) == length(v) || error("size mismatch")
@@ -156,6 +271,7 @@ function lanczos_sparse(h::SparseHamiltonian{Int64, T}, v::Vector{T}, g_out::Vec
 
     # generate the rest of the Lanczos basis
     for m = 2:info.qmin
+
         Ks.Q[:,m+1] = hoperation!(h, Ks.Q[:,m], Ks.Q[:,m+1])
         Ks.α[m] = dot(Ks.Q[:,m], Ks.Q[:,m+1])
 
@@ -169,6 +285,8 @@ function lanczos_sparse(h::SparseHamiltonian{Int64, T}, v::Vector{T}, g_out::Vec
 
         Ks.Q[:, m+1] -= Ks.α[m] .* Ks.Q[:, m] .+ Ks.β[m]*Ks.Q[:, m-1]
         Ks.β[m+1] = normalize!(Ks.Q, m+1)            
+
+        println("ground state energy=", energies[1])
 
         # explicit reorthogonalization of each newly generated state w.r.t. all 
         # other Lanczos vectors 
@@ -208,6 +326,7 @@ function lanczos_sparse(h::SparseHamiltonian{Int64, T}, v::Vector{T}, g_out::Vec
             else 
                 ene = e 
             end 
+            println("ground state energy=", energies[1])
 
             j += 1 
             # one Lanczos vector is added to the basis 
--- Lanczos_test.jl
+++ Lanczos_test.jl
@@ -2,7 +2,7 @@ using Test
 
 include("Lanczos.jl")
 
-@testset "6-site TIsing" begin 
+@testset "6-site TFIsing" begin 
     N = 6
     J = zeros(N, N)
     for i = 1:N-1
@@ -12,13 +12,42 @@ include("Lanczos.jl")
     J[1, N] = J[N, 1] = 1.0
 
     h = hinit(J, hx=0.1, hz=0.0)
-    info = LanczosInfo(4, 20, false, 1.0)
+    info = LanczosInfo(10, 200, false, 1.0)
     gamma = hoperation!(h, ones(2^N), ones(2^N))
-    (Ks, g_out) = lanczos_sparse(h, ones(2^N), gamma, info)
+    @time (Ks, g_out) = lanczos_reortho(h, ones(2^N), gamma, info)
     # measure ground state energy
     g2 = zeros(2^N)
     g2 = hoperation!(h, g_out, g2)
     GS_energy = sum(g2 .* g_out) / N
 
+    println("info=", info)
+    println("GS_energy=", GS_energy)
     @test isapprox(GS_energy, -1.0025016072757338, atol=1e-8 )
 end 
+
+@testset "twopass Lanczos 6-site TFIsing" begin 
+    N = 6
+    J = zeros(N, N)
+    for i = 1:N-1
+        J[i,i+1] = 1.0
+        J[i+1, i] = 1.0
+    end 
+    J[1, N] = J[N, 1] = 1.0
+
+    h = hinit(J, hx=0.1, hz=0.0)
+    info = LanczosInfo(10, 200, false, 1.0)
+    gamma = hoperation!(h, ones(2^N), ones(2^N))
+    @time g_out = lanczos_twopass(h, ones(2^N), gamma, info)  
+
+    # measure ground state energy
+    g2 = zeros(2^N)
+    g2 = hoperation!(h, g_out, g2)
+    GS_energy = sum(g2 .* g_out) / N
+
+    println("info=", info)
+    println("GS_energy=", GS_energy)
+    @test isapprox(GS_energy, -1.0025016072757338, atol=1e-8 )
+end 
+
+
+
